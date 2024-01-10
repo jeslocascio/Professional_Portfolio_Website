d2778bb35c4edb1d9c92c412f358abc016f379e7
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2024-01-10 17:39:56 -0500

    Working On CSS for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Homepage from './pages/Homepage';
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,62 @@
-
+import { Container, Nav, Navbar } from "react-bootstrap";
 import Logo from '../assets/Jesse_LoCascio_Logo.png';
 import "../styles/index.css";
 import "../styles/Header.css";
 
 function Header() {
   return (
-    <div>
-      <header>
-      <a href="#">
+    <>
+    <Navbar className="header" expand="md">
+      <Container>
+        <Navbar.Brand href="#">
         <img src={Logo} className="brandLogo" alt="Jesse LoCascio Logo" />
-      </a>
-      <div>
-        <ul className='headerNav'>
-          <li>
-            <a href="#">Home</a>
-          </li>
-            <li>
-              <a href="#About_Me">About Me</a>
-            </li>
-          <li>
-            <a href="#Portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="#Skills">Skills</a>
-          </li>
-          <li>
-            <a href="#Contact_Me">Contact Me</a>
-          </li>
-        </ul>
-      </div>
-    </header>
-    </div>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#about" className="nav-link">
+              About Me
+            </Nav.Link>
+            <Nav.Link href="#portfolio" className="nav-link">
+              Portfolio
+            </Nav.Link>
+            <Nav.Link href="#skills" className="nav-link">
+              Skills
+            </Nav.Link>
+            <Nav.Link href="#contact" className="nav-link">
+              Contact Me
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </>
+    // <div>
+    //   <header>
+    //   <a href="#">
+    //     <img src={Logo} className="brandLogo" alt="Jesse LoCascio Logo" />
+    //   </a>
+    //   <div>
+    //     <ul className='headerNav'>
+    //       <li>
+    //         <a href="#">Home</a>
+    //       </li>
+    //         <li>
+    //           <a href="#About_Me">About Me</a>
+    //         </li>
+    //       <li>
+    //         <a href="#Portfolio">Portfolio</a>
+    //       </li>
+    //       <li>
+    //         <a href="#Skills">Skills</a>
+    //       </li>
+    //       <li>
+    //         <a href="#Contact_Me">Contact Me</a>
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </header>
+    // </div>
   )
 }
 
