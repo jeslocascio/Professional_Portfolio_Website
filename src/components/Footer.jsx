c4d8613579dfb4b29a8fd954e7ce312431caa709
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2024-01-16 15:57:18 -0500

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,51 @@
-function Footer() {
-  return (
-    <div>
-      Footer
-    </div>
-  )
-}
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { IoDocumentTextOutline } from "react-icons/io5";
 
-export default Footer
+import "../styles/Footer.css";
+
+export default function Footer() {
+  const resume =
+    "#";
+  return (
+    <footer className="footer p-3 text-center">
+      <div className="container">
+        <div className="mb-0 d-flex justify-content-center">
+          <div className="footer-link">
+            <a
+              href="https://www.linkedin.com/in/jesse-locascio-6778a02a0/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2"
+            >
+              <FaLinkedin className="footer-icon" id="linkedin-icon" />
+            </a>
+            <div>Linkedin</div>
+          </div>
+          <div className="footer-link mx-2 mx-sm-2 mx-md-4">
+            <a
+              href="https://github.com/jeslocascio"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2"
+            >
+              <FaGithub className="footer-icon" id="github-icon" />
+            </a>
+            <div>Github</div>
+          </div>
+          <div className="footer-link">
+            <a
+              href={resume}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-2"
+            >
+              <IoDocumentTextOutline className="footer-icon" id="resume-icon" />
+            </a>
+            <div>Resume</div>
+          </div>
+        </div>
+        <p className="mb-0 pt-2 text-dark">© 2024 Jesse LoCascio</p>
+      </div>
+    </footer>
+  );
+}
